b31cf7215f92534906e4ef613402f208d6088918
Clarissa Mobley <clarissamobley@me.com>
2024-04-26 17:28:12 -0400

    Added to markdown, moved index items to match markdown

--- index.js
+++ index.js
@@ -1,54 +1,60 @@
 // Include packages needed for this application
 
 //Step One: Create Variables for npm packages
-const inquirer = require('inquirer');
-const fs = require('fs');
-const generateMarkdown = require('./utils/generateMarkdown');
-
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./generateMarkdown.js");
 
 // Create an array of questions for user input
 const questions = [
-        {
-            type: 'input',
-            message: "What is the title of your project?",
-            name: 'Project Title',
-        },
-        {
-            type: 'list',
-            message: 'Table of Contents',
-            choices: ['Description', 'Installation', 'Usage', 'License', 'Contributing', 'Tests', 'Questions'],
-            name: 'Table of Contents',
-        },
-        {
-            type: 'input',
-            message: 'Please describe your project.',
-            name: 'Description',
-        },
-        {
-           type: 'input',
-           message: 'Enter installation instructions',
-           name: 'Installation',
-        },
-        {
-            type: 'input',
-            message: 'Enter usage information',
-            name: 'Usage'
-        },
-        {
-            type: 'input',
-            message: 'Enter license information',
-            name: 'License',
-        },
-        {
-            type: 'input',
-            message: 'Enter who contributed to the project',
-            name: 'Contributing',
-        },
-        
-    ])
-    .then((response) => {
-        console.log(response);
-    });
+  {
+    type: "input",
+    message: "What is the title of your project?",
+    name: "title",
+  },
+  {
+    type: "checkbox",
+    message: "Please select which license was used for this project.",
+    choices: ("MIT", "Apache 2.0", "GPL 3.0", "None"),
+    name: "License",
+  },
+  {
+    type: "input",
+    message: "Please describe your project.",
+    name: "description",
+  },
+  {
+    type: "list",
+    message: "Table of Contents",
+    choices: [
+      "Description",
+      "Installation",
+      "Usage",
+      "License",
+      "Contributing",
+      "Tests",
+      "Questions",
+    ],
+    name: "Table of Contents",
+  },
+  {
+    type: "input",
+    message: "Enter installation instructions",
+    name: "Installation",
+  },
+  {
+    type: "input",
+    message: "Enter usage information",
+    name: "Usage",
+  },
+  {
+    type: "input",
+    message: "Enter who contributed to the project",
+    name: "Contributing",
+  },
+].then((response) => {
+  console.log(response);
+});
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -11,20 +11,22 @@ function renderLicenseBadge(license) {
     default:
       return '';
   }
-}
-renderLicenseBadge();
+};
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// // TODO: Create a function that returns the license link
+// // If there is no license, return an empty string
+// function renderLicenseLink(license) {}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// // TODO: Create a function that returns the license section of README
+// // If there is no license, return an empty string
+// function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ${renderLicenseBadge(data.license)}
+  ## Description
+  ${data.description}
 
 `;
 }
